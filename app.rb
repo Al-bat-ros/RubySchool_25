72bc8714928f7fa5818cdb33040e764f3a9c66e3
al-bat-ros <al-bat-ros@mail.ru>
2017-10-13 03:50:30 +0500

    Execute simple 2

--- app.rb
+++ app.rb
@@ -2,7 +2,17 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'bd'
+    #  Добавление в базу данных
+# db.execute  "INSERT INTO Cars (Name,Price) VALUES ('LADA',1100000000)"
 
-db.execute  "INSERT INTO Cars (Name,Price) VALUES ('LADA',1100000000)"
+# db.close
+
+# Преобразование Базы данных и вывод
+db.execute "SELECT * FROM Cars" do |car|
+
+	puts car
+	puts "===="
+
+end
 
 db.close
